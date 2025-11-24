ca6de4261b4908c76c1944b139e6af063c5582d7
wall-e785 <wall_echau604@yahoo.ca>
2025-11-24 13:19:03 -0800

    Added Interaction to 2009-2018

--- JS/vis.js
+++ JS/vis.js
@@ -1,7 +1,91 @@
+//global variables for responsiveness, referenced from: https://www.geeksforgeeks.org/javascript/how-to-detect-when-the-window-size-is-resized-using-javascript/
+let screenWidth = window.innerWidth;
+let visWidth = 400;
+let visHeight = 300;
+console.log(screenWidth);
+updateVisDims();
+
+function updateVisDims(){
+  if(screenWidth >= 1800){
+    visWidth = 800;
+  }
+}
+
+window.onresize = function(){
+  screenWidth = window.innerWidth;
+  console.log(screenWidth);
+
+  updateVisDims();
+}
+
+//global variables for 2009-2018 US Sales Visualization
+let visUSSales;
+let visUSData;
+
+//radio button selector for visUSSales, button to toggle between the three different views.
+//referenced: https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
+let selectedFormat = "Both";
+const button = document.getElementById("formatButton");
+const formatRadioButtons = document.querySelectorAll('input[name="formatHighlight"]');
+button.addEventListener("click", () => {
+  for (const radioButton of formatRadioButtons) {
+      if (radioButton.checked) {
+          //console.log(radioButton.value);
+
+          //create the visualization based on the selected option
+          selectedFormat = radioButton.value;
+          visUSSales = createVisUSSales(visUSData, selectedFormat);
+
+          //rerender the US Vis
+          runUSVis();
+          break;
+      }
+    }
+})
+
+//to implement the interactivity from observable, this function recreates the VisUSSales visualization using whichever format is seleted by HTML radio buttons.
+function createVisUSSales(usData, selectedFormat){
+  console.log("Creating US Vis");
+  return vl.markRect()
+    .data(
+      usData.flatMap(d => 
+        ["Physical", "Digital"].map(format => ({
+          Year: d.Year,
+          Format: format,
+          Sales: +d[format],  // ensure numeric
+          opacity: selectedFormat === "Both" || selectedFormat === format ? 1 : 0.2
+        }))
+      )
+    )
+    .encode(
+      vl.x().fieldO("Year"),
+      vl.y().fieldQ("Sales"),
+      vl.color().fieldN("Format").scale({range: ["#1f77b4", "#ff7f0e"]}), //put colour values here to pic them
+      vl.opacity().fieldQ("opacity"),
+      vl.tooltip([
+        {field: "Year", type: "ordinal"},
+        {field: "Format", type: "nominal"},
+        {field: "Sales", type: "quantitative"}
+      ])
+  )
+  .width(visWidth)
+  .height(490)
+  .toSpec(); 
+}
+
+//used as seperate run function to only render the 2009-2018 vis
+async function runUSVis(){
+  const usData = await d3.csv("./datasets/US_SalesData.csv");
+
+  visUSData = usData;
+
+  visUSSales = createVisUSSales(usData, selectedFormat);
+  await render("#visUSSales", visUSSales);
+}
+
 async function run() {
   const companies = await d3.csv("./datasets/chart1data format column v2@2.csv");
   const platforms = await d3.csv("./datasets/Single Platform (Console Developer) vs. Multi Platform.csv");
-  const usData = await d3.csv("./datasets/US_SalesData.csv");
 
   const visComparison = vl.markSquare()
     .data(platforms)
@@ -29,92 +113,52 @@ async function run() {
         stroke: "#ccc"            // optional border
       }
     })
-    .width(800)
+    .width(visWidth)
     .height(400)
     .toSpec(); 
 
 
-  const visRevenueShare = vl
-      .markBar()
-      .data(companies)
-      .title("Digital Revenue Share of Video Game Publishers Worldwide in 2024")
-      .encode(
-          vl.column().fieldN("Publisher").sort(vl.fieldQ("Frequency").order("ascending")).title(""),
-          vl.x().fieldN("Format").title(""),
-          vl.y().fieldQ("Frequency").axis({ format: "%"}).title("Percentage of Sales"),
-          vl.color().fieldN("Format"),
-      ).width(210).toSpec();
-    
-    //radio button selector for visUSSales
-    //referenced: https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
-    const button = document.getElementById("formatButton");
-    const formatRadioButtons = document.querySelectorAll('input[name="formatHighlight"]');
-    let selectedFormat;
-    button.addEventListener("click", () => {
-            for (const radioButton of formatRadioButtons) {
-                if (radioButton.checked) {
-                    selectedFormat = radioButton.value;
-                    break;
-                }
-              }
-    })
+  // const visRevenueShare = vl
+  //     .markBar()
+  //     .data(companies)
+  //     .title("Digital Revenue Share of Video Game Publishers Worldwide in 2024")
+  //     .encode(
+  //         vl.column().fieldN("Publisher").sort(vl.fieldQ("Frequency").order("ascending")).title(""),
+  //         vl.x().fieldN("Format").title(""),
+  //         vl.y().fieldQ("Frequency").axis({ format: "%"}).title("Percentage of Sales"),
+  //         vl.color().fieldN("Format"),
+  //     ).width(210).toSpec();
 
-    const visUSSales = vl.markRect()
-      .data(
-        usData.flatMap(d => 
-          ["Physical", "Digital"].map(format => ({
-            Year: d.Year,
-            Format: format,
-            Sales: +d[format],  // ensure numeric
-            opacity: selectedFormat === "Both" || selectedFormat === format ? 1 : 0.2
-          }))
-        )
-      )
-      .encode(
-        vl.x().fieldO("Year"),
-        vl.y().fieldQ("Sales"),
-        vl.color().fieldN("Format").scale({range: ["#1f77b4", "#ff7f0e"]}), //put colour values here to pic them
-        vl.opacity().fieldQ("opacity"),
-        vl.tooltip([
-          {field: "Year", type: "ordinal"},
-          {field: "Format", type: "nominal"},
-          {field: "Sales", type: "quantitative"}
-        ])
-    )
-    .width(1000)
-    .height(490)
-    .toSpec(); 
-
-  const visSquareEnixSales = vl.markBar()                        // Make a scatter chart
-    .data(
-      //filter control
-      platforms.filter(d =>
-        (d.Developer === "Square Enix")
-      )
-    )
-    .title("Square Enix Sales by Format")
-    .transform([ //transform the data into an array
-      vl.fold(["Physical (%)", "Digital (%)"])
-        .as(["Format", "Sales"]),
+  // const visSquareEnixSales = vl.markBar()                        // Make a scatter chart
+  //   .data(
+  //     //filter control
+  //     platforms.filter(d =>
+  //       (d.Developer === "Square Enix")
+  //     )
+  //   )
+  //   .title("Square Enix Sales by Format")
+  //   .transform([ //transform the data into an array
+  //     vl.fold(["Physical (%)", "Digital (%)"])
+  //       .as(["Format", "Sales"]),
     
-    ])
-    .encode(
-      vl.x().fieldO("Year"),       // Platform
-      vl.y().fieldQ("Sales"),              // Genre
-      //vl.size().fieldQ("Sales"),
-      vl.color().fieldN("Format"),
-    // vl.tooltip().fieldN("Developer"),
-      vl.tooltip([ //copied the tooltip from the previous visualization
-        {field: "Developer", type: "nominal"},
-        {field: "Format", type: "nominal"},
-        {field: "Sales", type: "quantitative"}
-        ]),
-      vl.yOffset().fieldN("Sales") // makes bars grouped by region
+  //   ])
+  //   .encode(
+  //     vl.x().fieldO("Year"),       // Platform
+  //     vl.y().fieldQ("Sales"),              // Genre
+  //     //vl.size().fieldQ("Sales"),
+  //     vl.color().fieldN("Format"),
+  //   // vl.tooltip().fieldN("Developer"),
+  //     vl.tooltip([ //copied the tooltip from the previous visualization
+  //       {field: "Developer", type: "nominal"},
+  //       {field: "Format", type: "nominal"},
+  //       {field: "Sales", type: "quantitative"}
+  //       ]),
+  //     vl.yOffset().fieldN("Sales") // makes bars grouped by region
       
-    )
-    .width(840)
-    .height(400)
-    .toSpec(); 
+  //   )
+  //   .width(800)
+  //   .height(400)
+  //   .toSpec(); 
 
   const visNintendoSales= vl.markBar()                        // Make a scatter chart
     .data(
@@ -143,49 +187,51 @@ async function run() {
       vl.yOffset().fieldN("Sales") // makes bars grouped by region
       
     )
-    .width(840)
+    .width(visWidth)
     .height(400)
     .toSpec();
 
-  const visCapcomSales = vl.markBar()                        // Make a scatter chart
-    .data(
-      //filter control
-      platforms.filter(d =>
-        (d.Developer === "Capcom")
-      )
-    )
-    .title("Capcom Sales by Format")
-    .transform([ //transform the data into an array
-      vl.fold(["Physical (%)", "Digital (%)"])
-        .as(["Format", "Sales"]),
+  // const visCapcomSales = vl.markBar()                        // Make a scatter chart
+  //   .data(
+  //     //filter control
+  //     platforms.filter(d =>
+  //       (d.Developer === "Capcom")
+  //     )
+  //   )
+  //   .title("Capcom Sales by Format")
+  //   .transform([ //transform the data into an array
+  //     vl.fold(["Physical (%)", "Digital (%)"])
+  //       .as(["Format", "Sales"]),
     
-    ])
-    .encode(
-      vl.x().fieldO("Year"),       // Platform
-      vl.y().fieldQ("Sales"),              // Genre
-      //vl.size().fieldQ("Sales"),
-      vl.color().fieldN("Format"),
-    // vl.tooltip().fieldN("Developer"),
-      vl.tooltip([ //copied the tooltip from the previous visualization
-        {field: "Developer", type: "nominal"},
-        {field: "Format", type: "nominal"},
-        {field: "Sales", type: "quantitative"}
-        ]),
-      vl.yOffset().fieldN("Sales") // makes bars grouped by region
+  //   ])
+  //   .encode(
+  //     vl.x().fieldO("Year"),       // Platform
+  //     vl.y().fieldQ("Sales"),              // Genre
+  //     //vl.size().fieldQ("Sales"),
+  //     vl.color().fieldN("Format"),
+  //   // vl.tooltip().fieldN("Developer"),
+  //     vl.tooltip([ //copied the tooltip from the previous visualization
+  //       {field: "Developer", type: "nominal"},
+  //       {field: "Format", type: "nominal"},
+  //       {field: "Sales", type: "quantitative"}
+  //       ]),
+  //     vl.yOffset().fieldN("Sales") // makes bars grouped by region
       
-    )
-    .width(950)
-    .height(600)
-    .toSpec() 
+  //   )
+  //   .width(950)
+  //   .height(600)
+  //   .toSpec() 
  
 
 
   await render("#visComparison", visComparison);
   // await render("#visRevenueShare", visRevenueShare);
-  await render("#visUSSales", visUSSales);
+
   //await render("#visSquareEnixSales", visSquareEnixSales);
   await render("#visNintendoSales", visNintendoSales);
   // await render("#visCapcomSales", visCapcomSales);
+
+  runUSVis();
 
 }
 
